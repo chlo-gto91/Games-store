05da653ec710101aeb9f72c76a649dfb1ca33b0e
llanelli13 <pierre.rey@efrei.net>
2022-10-20 19:02:06 +0200

    Update DataBase

--- DataBase.sql
+++ DataBase.sql
@@ -49,29 +49,29 @@ CREATE TABLE game(
    FOREIGN KEY(ID_client) REFERENCES client(ID_client)
 );
 
-insert into client values (NULL,"20","Gattino","Chloé","0625126117","Mario kart 8","chloegattino5@gmail.com");
-insert into client values (NULL,"17","Cantrelle","Noa","0785896324","Wii sport resort","noacantrelle@efrei.net");
-insert into client values (NULL,"31","Rey","Pierre","0698741236","Assassin's Creed Origins","pierre.rey@efrei.net");
-insert into client values (NULL,"14","Gattino","Manon","0652741670","Mario kart 8","gattinomanon@gmail.com");
-insert into client values (NULL,"25","Michot","Marie","0766984610","Minecraft","marie.michot@efrei.net");
-insert into client values (NULL,"38","Harry","Potter","0620000000","Fifa 23","Harry.potter@efrei.net");
-insert into client values (NULL,"45","Chapron","Nathalie","0645987632","Animal Crossing","nathalie.chapron@efrei.net");
-insert into client values (NULL,"18","Hecq","Guerlyn","0699765213","NBA 2K23","guerlynhecq@gmail.com");
-insert into client values (NULL,"17","Pinto","Martin","0755264890","Tennis manager 2022","martin.pinto@gmail.com");
-insert into client values (NULL,"14","Gattino","Manon","0652741670","Animal Crossing","gattinomanon@gmail.com");
-insert into client values (NULL,"22","Prim","Lorraine","0650020491","Rayman Legends Definitive","primlorraine@gmail.com");
+insert into client values (NULL,20,"Gattino","Chloé","0625126117","Mario kart 8","chloegattino5@gmail.com");
+insert into client values (NULL,17,"Cantrelle","Noa","0785896324","Wii sport resort","noacantrelle@efrei.net");
+insert into client values (NULL,31,"Rey","Pierre","0698741236","Assassin's Creed Origins","pierre.rey@efrei.net");
+insert into client values (NULL,14,"Gattino","Manon","0652741670","Mario kart 8","gattinomanon@gmail.com");
+insert into client values (NULL,25,"Michot","Marie","0766984610","Minecraft","marie.michot@efrei.net");
+insert into client values (NULL,38,"Harry","Potter","0620000000","Fifa 23","Harry.potter@efrei.net");
+insert into client values (NULL,45,"Chapron","Nathalie","0645987632","Animal Crossing","nathalie.chapron@efrei.net");
+insert into client values (NULL,18,"Hecq","Guerlyn","0699765213","NBA 2K23","guerlynhecq@gmail.com");
+insert into client values (NULL,17,"Pinto","Martin","0755264890","Tennis manager 2022","martin.pinto@gmail.com");
+insert into client values (NULL,14,"Gattino","Manon","0652741670","Animal Crossing","gattinomanon@gmail.com");
+insert into client values (NULL,22,"Prim","Lorraine","0650020491","Rayman Legends Definitive","primlorraine@gmail.com");
 
-insert into store values (NULL,"20","81 route de Saint Germain, 78640", "4","Gameshop");
+insert into store values (NULL,20,"81 route de Saint Germain, 78640", "4","Gameshop");
 
-insert into game values (NULL,"55","frenzied karting races in the universe of super mario", "Mario kart 8");
-insert into game values (NULL,"48","Family sports game", "Wii sport resort");
-insert into game values (NULL,"30","action-adventure open-world stealth game", "Assassin's Creed Origins");
-insert into game values (NULL,"20","players explore a blocky, procedurally generated 3D world with virtually infinite terrain and may discover and extract raw materials", "Minecraft");
-insert into game values (NULL,"70","football game that allows the player to develop his favorite team", "Fifa 23");
-insert into game values (NULL,"52","series of life simulation games in which players control an avatar who starts a new life, in a city full of eccentric animals", "Animal Crossing");
-insert into game values (NULL,"45","basketball game based on the National Basketball Association", "NBA 2K23");
-insert into game values (NULL,"49","tennis management game", "Tennis manager 2022");
-insert into game values (NULL,"30","2D side-scrolling platform video game", "Rayman Legends Definitive");
+insert into game values (NULL,55,"frenzied karting races in the universe of super mario", "Mario kart 8");
+insert into game values (NULL,48,"Family sports game", "Wii sport resort");
+insert into game values (NULL,30,"action-adventure open-world stealth game", "Assassin's Creed Origins");
+insert into game values (NULL,20,"players explore a blocky, procedurally generated 3D world with virtually infinite terrain and may discover and extract raw materials", "Minecraft");
+insert into game values (NULL,70,"football game that allows the player to develop his favorite team", "Fifa 23");
+insert into game values (NULL,52,"series of life simulation games in which players control an avatar who starts a new life, in a city full of eccentric animals", "Animal Crossing");
+insert into game values (NULL,45,"basketball game based on the National Basketball Association", "NBA 2K23");
+insert into game values (NULL,49,"tennis management game", "Tennis manager 2022");
+insert into game values (NULL,30,"2D side-scrolling platform video game", "Rayman Legends Definitive");
 
 insert into editor values (NULL,"Electronic Arts"); --Fifa
 insert into editor values (NULL,"Nitendo"); -- Mario, Animal crossing, Wii sport
