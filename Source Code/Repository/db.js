ad6f68c2f900cc617d7f052be03c1bcf646f659a
chloé Gattino <chloe.gattino@efrei.net>
2022-12-02 11:53:12 +0100

    db

--- Source Code/Repository/db.js
+++ Source Code/Repository/db.js
@@ -1,8 +1,8 @@
 // utils/db.js
 
-const mysql = require("mysql2/promise");
+const mariadb = require("mariadb");
 
-const pool = mysql.createPool({
+const pool = mariadb.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
