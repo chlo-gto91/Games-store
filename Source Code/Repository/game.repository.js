9cb1615730c97ff6f9bb25887750b2d8ae19a183
chloé Gattino <chloe.gattino@efrei.net>
2022-12-08 10:31:28 +0100

    delete done

--- Source Code/Repository/game.repository.js
+++ Source Code/Repository/game.repository.js
@@ -170,9 +170,9 @@ module.exports = {
         try {
             let conn = await pool.getConnection();
             let sql = "DELETE FROM have WHERE ID_game=?";
-            const okPacket = await conn.query(sql, game_ID);
+        
             sql = "DELETE FROM game WHERE ID_game=?";
-            okPacket = await conn.query(sql, game_ID);
+            const okPacket = await conn.query(sql, game_ID);
             conn.end();
             console.log(okPacket);
             return okPacket.affectedRows;
